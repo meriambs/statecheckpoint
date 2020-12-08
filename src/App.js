f65186a1155ffedd3f92b9af8bbf282da8c8e319
meriambs <bensalah.meriem0@gmail.com>
2020-12-09 00:47:59 +0100

    almost css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Increment from './compoment/Increment';
 import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import{Navbar,Nav,Form,FormControl,Button,} from 'react-bootstrap';
+import { BsFillHouseFill } from "react-icons/bs";
+import { BsBriefcase } from "react-icons/bs";
+
+import { SiGithub } from "react-icons/si";
+import { DiJsBadge }from "react-icons/di";
 
  const buttonn={
        background: "#b1c3e0",
-    color: "#272323",
    width: "203px",
      height: "49px",
    borderRadius: "18px",
@@ -16,6 +22,12 @@ import React, {Component} from 'react';
      justifyContent: "center",
      marginTop: "143px",
  }
+  const partiphoto={
+     height: "225px",
+    marginTop: "-46px",
+  borderRadius: "109px",
+  marginLeft: "594px",
+ }
 
 
 class App extends Component {
@@ -27,7 +39,7 @@ class App extends Component {
           profile:{
             fullName:"Ben Salah Meriam",
             bio:"everything and nothing",
-            imgSrc:<img src="./img/photo-de-profil.jpg"/>,
+            imgSrc:<img style={partiphoto} src="./img/photo-de-profil.jpg"/>,
             profession:"ingenieur en bioindus",
                   }
       };
@@ -41,7 +53,21 @@ class App extends Component {
   render() {
     const { showCounter } = this.state;
     return (
-    <div className="App" style={headcss} >
+      
+   <div>
+         <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Profil</Navbar.Brand>
+        <Nav className="mr-auto">
+         <Nav.Link href="#home">Home</Nav.Link>
+         <Nav.Link href="#features">First Articl</Nav.Link>
+        <Nav.Link href="#pricing">Second Articl</Nav.Link>
+        </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+      </Navbar>
+       <div className="App" style={headcss} >
       <header className="App-header">
         
         <button style={buttonn} onClick={this.toggleShow.bind(this)}>toggle counter</button>
@@ -51,6 +77,33 @@ class App extends Component {
           }
       
       </header>
+    </div>
+    <div style={iconpart} >
+     <div style={mailpart} >
+        <div  style={badg}  >
+       <BsBriefcase ></BsBriefcase>
+       </div>
+      <h5  >bensalah.meriem0@gmail.com</h5>
+      </div>
+      <div style={mailpart}   >
+        <div  style={badg}  >
+          <a  href="https://github.com/meriambs" >
+         <SiGithub/>
+         </a>
+         </div>
+      <h5  >gitpart</h5>
+      </div>
+      <div style={mailpart}  >
+        <div  style={badg}  >
+          <a  href="https://www.codewars.com/users/MeriambenS">
+        <DiJsBadge/>
+        </a>
+        </div>
+      <h5  >codewars</h5>
+      </div>
+     
+  
+   </div>
     </div>
   );
 }
--- src/compoment/Increment.js
+++ src/compoment/Increment.js
@@ -16,11 +16,10 @@ const uphed={
     marginLeft: "125px",
  }
  const partiephotoettext={
-         border: "solid 3px gray",
-     borderRadius: "8px",
-   
-     height: "22px",
+        fontSize: "39px",
+    lineHeight:"53px",
  }
+
 class Increment extends Component {
 
     constructor(props) {
@@ -38,48 +37,25 @@ class Increment extends Component {
         }, 1000);
     }
     
-//        state = {
-//      fullName:"Ben Salah Meriam",
-//      bio:"everything and nothing",
-//      imgSrc:<img src="./img/photo-de-profil.jpg"/>,
-//     profession:"ingenieur en bioindus",
-
-//    }
-//       }
     render() {
         const { counter } = this.state;
         const {profile}=this.props;
         return (
             <div style={uphed} >
+              
                 <div style={counterdiv}>
                 {counter} secondes ont passées
                 </div>
                 <div style={scdiv} >
-                <h2> hello <br/> i will present my self <br/>
-          I'm {profile.fullName} <br/> my bio : { profile.bio} 
-          i'm a {profile.profession }<br/> {profile.imgSrc} </h2>
+                <h2 style={partiephotoettext} > Hello <br/> I will present my self : <br/>
+          I'm {profile.fullName} <br/>
+          i'm a {profile.profession }<br/>
+          -----------------------------<br/>
+          My bio : { profile.bio} <br/> {profile.imgSrc} </h2>
        </div>
                 </div>
         )
         }
     }
-//       handleClick=()=>this.setState({fullName:"Aya", bio:"laugh",imgSrc:<img src="./img/photo-de-prof.jpg"/>,profession:"Webdevp"})
-    
-//     render()
-//     {    
-//          return (     <div className="App">              <header className="App-header">           <parent/>
-//             <h2> hello <br/> i will present my self <br/>
-//          I'm {this.state.fullName} <br/> my bio : { this.state.bio} 
-//            <br/>{this.state.imgSrc} <br/> and i'm a {this.state.profession}</h2>
-//         <p>click to discover our guest </p>
-//          <button onClick={this. handleClick}> ClickMe</button>
-//       </header>
-//       </div>
-//         );
-//    }
- 
-
- 
-
 
 export default Increment;
